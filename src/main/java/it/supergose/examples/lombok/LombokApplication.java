b17d9951a0a3268d7f671a415b1eae6fb7873fca
Marcello Tosetti <marcello.tosetti@gmail.com>
2018-03-30 10:24:54 +0200

    new Spring Boot RESTful Web Service

---
+++ src/main/java/it/supergose/examples/lombok/LombokApplication.java
@@ -0,0 +1,12 @@
+package it.supergose.examples.lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LombokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LombokApplication.class, args);
+	}
+}
